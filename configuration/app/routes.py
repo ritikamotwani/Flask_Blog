e1b422e3d35f821c0231ce5dcb0fd36359ab878d
ritikamotwani <iit2015096@iiita.ac.in>
2017-12-17 22:08:04 +0530

    Methods to log in

--- configuration/app/routes.py
+++ configuration/app/routes.py
@@ -18,8 +18,12 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+		return redirect('/index')
+
 	return render_template('login.html', title = 'Sign In', form = form)
 
